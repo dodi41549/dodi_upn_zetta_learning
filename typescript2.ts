92be22525d9fb575627e70800d2a89b0fbc995ca
dodi41549 <101625141+dodi41549@users.noreply.github.com>
2022-10-26 16:37:12 +0700

    initial commit

---
+++ typescript2.ts
@@ -0,0 +1,8 @@
+function greet(person: { number1: number; word: string; number2: number; words: string; }) {
+    return 1 + 'data' + 3 + 'result';
+  }
+greet
+function second(person1: { word1: string; word2: string; number3: number; word3: string; word4: string;}) {
+    return 'Bejo' + 'has'+ '4' + 'sport' + 'car';
+  }
+second
